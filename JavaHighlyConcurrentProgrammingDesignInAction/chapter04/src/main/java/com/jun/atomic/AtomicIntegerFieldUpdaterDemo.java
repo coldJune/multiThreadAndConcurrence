17c27f8fe5a38262994fb429c3d7d8a92a2e587b
coldJune <785691557@qq.com>
2020-01-10 14:41:04 +0800

    [Java Highly Concurrent Programming Design In Action] Atomic Updater comment

--- JavaHighlyConcurrentProgrammingDesignInAction/chapter04/src/main/java/com/jun/atomic/AtomicIntegerFieldUpdaterDemo.java
+++ JavaHighlyConcurrentProgrammingDesignInAction/chapter04/src/main/java/com/jun/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -19,6 +19,11 @@ public class AtomicIntegerFieldUpdaterDemo {
      * AtomicIntegerFieldUpdater可以在不改动原有代码的基础上，让普通的变量享受CAS操作带来的线程安全性
      *
      * 最后allscore的分数和score的分数一致，说明线程安全得到保证
+     *
+     * 注意事项
+     * 1.Updater只能修改它可见范围内的变量，因为Updater使用反射得到这个变量(比如不能是private)
+     * 2.为了确保变量被正确读取，它必须是volatile类型
+     * 3.由于CAS操作会通过对象实例中的偏移量直接进行赋值，因此它不支持static类型
      * @param args
      * @throws InterruptedException
      */
